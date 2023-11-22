7679d4a700f8581897d7f4498519501e0dfa4ec3
Krishna Sivapothanathan <krishnasivapothanathan@gmail.com>
2023-11-22 08:02:26 +0530

    script tag updated for toggle class

--- template.js
+++ template.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.menu-toggle').on('click',function(){
-        $('.nav').toggleClass('showing');
+    $('.nav').toggleClass('showing');
+    $('.nav ul').toggleClass('showing');
     });    
 });
